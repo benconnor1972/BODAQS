04f6b0f1b30e72e2b46b8a2330d0149d7dac8338
George Connor <grgcnnr@gmail.com>
2026-04-03 19:29:02 +1300

    scaffold astro docs site.
    - Add some initial content (still needs images)
    - Fist shot at a gh action to publish the site (unable to test before merging)

---
+++ bodocs/astro.config.mjs
@@ -0,0 +1,27 @@
+// @ts-check
+import {defineConfig} from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+export default defineConfig({
+  site: 'https://bodaqs.net',
+  integrations: [
+    starlight({
+      title: 'Bodocs',
+      logo: {
+        replacesTitle:true,
+        light: './src/assets/logo-light.svg',
+        dark: './src/assets/logo-dark.svg',
+      },
+      customCss: [
+        './src/styles/tokens.css',
+      ],
+      social: [{icon: 'github', label: 'GitHub', href: 'https://github.com/benconnor1972/BODAQS'}],
+      sidebar: [
+        {
+          label: 'Hardware guide',
+          autogenerate: {directory: 'hardware-guide'},
+        },
+      ],
+    }),
+  ],
+});
