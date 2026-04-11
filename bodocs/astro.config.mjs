18c58557f25420bf222cc8ce923b668fc65eaf84
George Connor <grgcnnr@gmail.com>
2026-04-11 17:13:30 +1200

    add ms Clarity tracking

--- bodocs/astro.config.mjs
+++ bodocs/astro.config.mjs
@@ -20,6 +20,19 @@ export default defineConfig({
         {icon: 'discord', label: 'Discord', href: 'https://discord.gg/BkWuT4S5kB'},
         {icon: 'instagram', label: 'Discord', href: 'https://www.instagram.com/bodaqs'}
       ],
+      head: [
+
+        {
+          tag: 'script',
+          content: `
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "w9v4spkbez");
+          `,
+        },
+      ],
       sidebar: [
         {
           label: 'Hardware guide',
